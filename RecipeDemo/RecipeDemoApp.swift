219aa06b2feadbf2aac01a458530a3409689c98d
MP Data - Trading <42841276+MPiccato@users.noreply.github.com>
2024-01-20 07:35:41 -0300

    Initial Commit

---
+++ RecipeDemo/RecipeDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  RecipeDemoApp.swift
+//  RecipeDemo
+//
+//  Created by Martin Piccato on 20/01/2024.
+//
+
+import SwiftUI
+
+@main
+struct RecipeDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
